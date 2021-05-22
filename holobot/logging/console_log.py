4d71ba5baf674a64bcd81d725d8ac4480ea95c6f
Rexor <rrexor@gmail.com>
2021-05-22 17:04:30 +0200

    Modify some logs to be trace level

--- holobot/logging/console_log.py
+++ holobot/logging/console_log.py
@@ -5,10 +5,10 @@ from holobot.logging.log_level import LogLevel
 from typing import Optional
 
 class ConsoleLog(LogBase):
-    def __init__(self, service_collection: ServiceCollectionInterface):
+    def __init__(self, service_collection: ServiceCollectionInterface) -> None:
         super().__init__(service_collection)
 
-    def _on_write(self, level: LogLevel, message: str, error: Optional[Exception]):
+    def _on_write(self, level: LogLevel, message: str, error: Optional[Exception]) -> None:
         final_message = f"[{level.name}] [{datetime.now():%Y%m%dT%H%M%S}] {message}"
         if error is not None:
             final_message += f"\n{error}"
--- holobot/logging/log_base.py
+++ holobot/logging/log_base.py
@@ -4,31 +4,31 @@ from holobot.logging.log_level import LogLevel
 from typing import Optional
 
 class LogBase(LogInterface):
-    def __init__(self, service_collection: ServiceCollectionInterface):
+    def __init__(self, service_collection: ServiceCollectionInterface) -> None:
         self.log_level = LogLevel.INFORMATION
 
-    def write(self, level: LogLevel, message: str, error: Optional[Exception] = None):
+    def write(self, level: LogLevel, message: str, error: Optional[Exception] = None) -> None:
         if level < self.log_level:
             return
         self._on_write(level, message, error)
 
-    def trace(self, message: str):
+    def trace(self, message: str) -> None:
         self.write(LogLevel.TRACE, message)
 
-    def debug(self, message: str):
+    def debug(self, message: str) -> None:
         self.write(LogLevel.DEBUG, message)
 
-    def info(self, message: str):
+    def info(self, message: str) -> None:
         self.write(LogLevel.INFORMATION, message)
 
-    def warning(self, message: str):
+    def warning(self, message: str) -> None:
         self.write(LogLevel.WARNING, message)
 
-    def error(self, message: str, error: Exception):
+    def error(self, message: str, error: Exception) -> None:
         self.write(LogLevel.ERROR, message, error)
 
-    def critical(self, message: str, error: Exception):
+    def critical(self, message: str, error: Exception) -> None:
         self.write(LogLevel.CRITICAL, message, error)
     
-    def _on_write(self, level: LogLevel, message: str, error: Optional[Exception]):
+    def _on_write(self, level: LogLevel, message: str, error: Optional[Exception]) -> None:
         raise NotImplementedError

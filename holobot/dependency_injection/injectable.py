3bfa6ad91708c171792878ddf9ae631b52a05957
Rexor <rrexor@gmail.com>
2021-05-22 21:09:36 +0200

    Add generic type hint to injectable

--- holobot/dependency_injection/injectable.py
+++ holobot/dependency_injection/injectable.py
@@ -1,8 +1,10 @@
 from .models import ExportMetadata
-from typing import Any, Callable, List, Type
+from typing import Any, Callable, List, Type, TypeVar
 
-def injectable(contract_type: Type[Any]) -> Callable[[Type[Any]], Type[Any]]:
-    def decorator(wrapped_class: Type[Any]) -> Type[Any]:
+T = TypeVar("T")
+
+def injectable(contract_type: Type[Any]) -> Callable[[Type[T]], Type[T]]:
+    def decorator(wrapped_class: Type[T]) -> Type[T]:
         metadatas: List[ExportMetadata] = getattr(wrapped_class, ExportMetadata.PROPERTY_NAME, [])
         metadatas.append(ExportMetadata(contract_type, wrapped_class))
         setattr(wrapped_class, ExportMetadata.PROPERTY_NAME, metadatas)

78f4fccebd30ed66e03c5d25c1ad0586f825096a
Rexor <rrexor@gmail.com>
2021-06-19 19:13:16 +0200

    Implement a slash command for hentai links and add support for e-hentai

--- holobot/extensions/hentai/cogs/main.py
+++ holobot/extensions/hentai/cogs/main.py
@@ -4,10 +4,21 @@ from discord.ext.commands.cooldowns import BucketType
 from discord.ext.commands.core import cooldown, group
 from discord.ext.commands.errors import CommandInvokeError, CommandOnCooldown, MissingRequiredArgument
 from discord.message import DeletedReferencedMessage, Message, MessageReference
+from discord_slash import cog_ext, SlashContext
+from discord_slash.model import SlashCommandOptionType
+from discord_slash.utils.manage_commands import create_choice, create_option
 from holobot.discord.bot import Bot
+from holobot.discord.sdk.utils import reply
 from holobot.sdk.logging import LogInterface
 from holobot.sdk.utils import try_parse_int
 from typing import Optional
+
+import re
+
+NHENTAI_SITE_ID = "nhentai"
+EHENTAI_SITE_ID = "ehentai"
+
+ehentai_regex = re.compile(r"^(?:\d+)\/(?:[a-z0-9]{10})$")
 
 class Hentai(Cog, name="Hentai"):
     def __init__(self, bot: Bot):
@@ -19,6 +30,30 @@ class Hentai(Cog, name="Hentai"):
     async def hentai(self, context: Context):
         if not context.invoked_subcommand:
             await context.reply("You have to specify a sub-command!", delete_after=3)
+
+    @cog_ext.cog_subcommand(base="hentai", name="link", description="Gives you a link to a title on a specific site.", options=[
+        create_option("site", "The name of the site.", SlashCommandOptionType.STRING, True, choices=[
+            create_choice(NHENTAI_SITE_ID, "nhentai"),
+            create_choice(EHENTAI_SITE_ID, "e-hentai")
+        ]),
+        create_option("id", "The identifier of the title.", SlashCommandOptionType.STRING, True)
+    ])
+    async def slash_remove(self, context: SlashContext, site: str, id: str):
+        if not site or not id:
+            await reply(context, "You must specify both the site and the identifier.")
+            return
+        id = id.strip()
+        if site == NHENTAI_SITE_ID:
+            if (value := try_parse_int(id)) is None:
+                await reply(context, "Invalid identifier. A valid identifier, for example, is _206981_.")
+                return
+            await reply(context, f"https://nhentai.net/g/{value}")
+        elif site == EHENTAI_SITE_ID:
+            if not ehentai_regex.match(id):
+                await reply(context, "Invalid identifier. A valid identifier, for example, is _1383239/0e7abac58a_ (case-sensitive).")
+                return
+            await reply(context, f"https://e-hentai.org/g/{id}")
+        else: await reply(context, "You've specified an invalid site.")
     
     @cooldown(1, 10, BucketType.user)
     @hentai.command(aliases=["nh"], brief="Turns a number into an nhentai URL.")

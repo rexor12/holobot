555563d8c4e442d2ae7fbc186bfd5f7e82c59230
Rexor <rrexor@gmail.com>
2021-07-14 16:54:50 +0200

    Review changes

--- __main__.py
+++ __main__.py
@@ -1,10 +1,7 @@
 from holobot.framework import Kernel
 from holobot.framework.ioc import DependencyResolver, ServiceDiscovery
 
-import logging
-
 if __name__ == "__main__":
-	logging.basicConfig(filename="holobot.log", level=logging.DEBUG)
 	# The idea here is to register the services for each extension independently,
 	# however, today it doesn't make sense as they're still in the same package.
 	# Therefore, for now we just register everything from the entire package.

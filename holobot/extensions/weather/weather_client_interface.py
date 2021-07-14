616c93e4482d26d1a796a9e418e6f20a46f11046
Rexor <rrexor@gmail.com>
2021-07-14 18:22:52 +0200

    Refine error handling

--- holobot/extensions/weather/weather_client_interface.py
+++ holobot/extensions/weather/weather_client_interface.py
@@ -1,5 +1,5 @@
 from .models import WeatherData
 
 class WeatherClientInterface:
-    async def get_weather_data(self, city_name: str) -> WeatherData:
+    async def get_weather_data(self, location: str) -> WeatherData:
         raise NotImplementedError

ac40df45d7dfe5bb36dd919a8e761541d0bfd8fa
Rexor <rrexor@gmail.com>
2022-07-22 12:03:50 +0200

    Support unknown emojis by mention

--- holobot/discord/data_providers/emoji_data_provider.py
+++ holobot/discord/data_providers/emoji_data_provider.py
@@ -1,24 +1,32 @@
-from ..bot import BotAccessor
-from ..transformers.emoji import to_model
-from hikari import Emoji as HikariEmoji
-from holobot.discord.sdk.data_providers import IEmojiDataProvider
-from holobot.discord.sdk.models import Emoji
-from holobot.sdk.ioc.decorators import injectable
-from typing import Optional
+from typing import Match, Optional, Union
 
 import re
 
-EMOJI_REGEX = re.compile(r"^<(?:\:[aA])?\:(?:\w+)\:(?P<id>\d+)>$")
+from hikari import urls, Emoji as HikariEmoji
+from hikari.internal import routes
+
+from ..bot import BotAccessor
+from ..transformers.emoji import to_model
+from holobot.discord.sdk.data_providers import IEmojiDataProvider
+from holobot.discord.sdk.models import Emoji
+from holobot.sdk.ioc.decorators import injectable
+
+EMOJI_REGEX = re.compile(r"<(?P<flags>[^:]*):(?P<name>[^:]*):(?P<id>\d+)>")
 
 @injectable(IEmojiDataProvider)
 class EmojiDataProvider(IEmojiDataProvider):
     async def find_emoji(self, name_or_mention: str) -> Optional[Emoji]:
-        return to_model(emoji) if (emoji := await self.__find_emoji(name_or_mention)) else None
+        emoji = await self.__find_emoji(name_or_mention)
+        if isinstance(emoji, Emoji):
+            return emoji
 
-    async def __find_emoji(self, name_or_mention: str) -> Optional[HikariEmoji]:
-        # TODO Doesn't find emojis from other servers.
+        return to_model(emoji) if emoji else None
+
+    async def __find_emoji(self, name_or_mention: str) -> Union[HikariEmoji, Emoji, None]:
         if (match := EMOJI_REGEX.match(name_or_mention)) is not None:
-            return BotAccessor.get_bot().cache.get_emoji(int(match["id"]))
+            if emoji := BotAccessor.get_bot().cache.get_emoji(int(match["id"])):
+                return emoji
+            else: return EmojiDataProvider.__create_unknown_emoji(match, name_or_mention)
 
         name_or_mention = name_or_mention.lower()
         return next((
@@ -26,4 +34,20 @@ class EmojiDataProvider(IEmojiDataProvider):
                 if emoji.name.lower() == name_or_mention
             ),
             None
+        )
+
+    @staticmethod
+    def __create_unknown_emoji(emoji_match: Match[str], mention: str) -> Emoji:
+        is_animated = emoji_match["flags"] in ("a", "A")
+        extension = "gif" if is_animated else "png"
+        identifier = emoji_match["id"]
+        return Emoji(
+            identifier=None,
+            url=routes.CDN_CUSTOM_EMOJI.compile(
+                urls.CDN_URL,
+                emoji_id=identifier,
+                file_format=extension
+            ),
+            mention=mention,
+            is_known=False
         )
--- holobot/discord/sdk/models/emoji.py
+++ holobot/discord/sdk/models/emoji.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
 from typing import Optional
 
-@dataclass
+@dataclass(kw_only=True)
 class Emoji:
     identifier: Optional[str]
     url: Optional[str]
     mention: Optional[str]
+    is_known: bool

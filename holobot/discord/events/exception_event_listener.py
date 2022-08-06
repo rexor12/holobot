26b8b92eb35a529dcfda61d2b1cf1094e0676632
Rexor <rrexor@gmail.com>
2022-08-07 01:13:50 +0200

    Transform old event handlers to Discord event listeners

---
+++ holobot/discord/events/exception_event_listener.py
@@ -0,0 +1,32 @@
+from typing import Any, Type
+
+import hikari
+
+from .discord_event_listener_base import DiscordEventListenerBase
+from .igeneric_discord_event_listener import IGenericDiscordEventListener
+from holobot.discord.bot import Bot
+from holobot.sdk.ioc.decorators import injectable
+from holobot.sdk.logging import ILoggerFactory
+
+_EVENT_TYPE = hikari.ExceptionEvent
+
+@injectable(IGenericDiscordEventListener)
+class ExceptionEventListener(DiscordEventListenerBase[_EVENT_TYPE]):
+    def __init__(
+        self,
+        logger_factory: ILoggerFactory,
+    ) -> None:
+        super().__init__()
+        self.__logger = logger_factory.create(ExceptionEventListener)
+
+    @property
+    def event_type(self) -> Type[Any]:
+        return _EVENT_TYPE
+
+    async def on_event(self, bot: Bot, event: _EVENT_TYPE) -> None:
+        self.__logger.error(
+            "An event handler has raised an error.",
+            event.exception,
+            exception_type=type(event.exception).__name__,
+            failed_event=event.failed_event
+        )
---
+++ holobot/discord/events/starting_event_listener.py
@@ -0,0 +1,62 @@
+from typing import Any, Dict, List, Type
+
+from hikari.api.special_endpoints import CommandBuilder
+
+import hikari
+
+from .discord_event_listener_base import DiscordEventListenerBase
+from .igeneric_discord_event_listener import IGenericDiscordEventListener
+from holobot.discord.bot import Bot
+from holobot.discord.workflows import IWorkflowRegistry
+from holobot.sdk.configs import ConfiguratorInterface
+from holobot.sdk.diagnostics import DebuggerInterface
+from holobot.sdk.ioc.decorators import injectable
+from holobot.sdk.logging import ILoggerFactory
+from holobot.sdk.utils import get_or_add
+
+_EVENT_TYPE = hikari.StartingEvent
+
+@injectable(IGenericDiscordEventListener)
+class StartingEventListener(DiscordEventListenerBase[_EVENT_TYPE]):
+    def __init__(
+        self,
+        configurator: ConfiguratorInterface,
+        debugger: DebuggerInterface,
+        logger_factory: ILoggerFactory,
+        workflow_registry: IWorkflowRegistry
+    ) -> None:
+        super().__init__()
+        self.__debugger = debugger
+        self.__developer_server_id: int = configurator.get("Development", "DevelopmentServerId", 0)
+        self.__logger = logger_factory.create(StartingEventListener)
+        self.__workflow_registry = workflow_registry
+
+    @property
+    def event_type(self) -> Type[Any]:
+        return _EVENT_TYPE
+
+    async def on_event(self, bot: Bot, event: _EVENT_TYPE) -> None:
+        application = await bot.rest.fetch_application()
+        command_builders: Dict[str, List[CommandBuilder]] = {}
+        for server_id, builders in self.__workflow_registry.get_command_builders(bot).items():
+            cb = get_or_add(command_builders, server_id, lambda _: list[CommandBuilder](), None)
+            cb.extend(builders)
+        for server_id, builders in self.__workflow_registry.get_menu_item_builders(bot).items():
+            cb = get_or_add(command_builders, server_id, lambda _: list[CommandBuilder](), None)
+            cb.extend(builders)
+
+        if self.__debugger.is_debug_mode_enabled():
+            if str(self.__developer_server_id) in command_builders:
+                cb = get_or_add(command_builders, "", lambda _: list[CommandBuilder](), None)
+                cb.extend(command_builders.pop(str(self.__developer_server_id)))
+
+        for server_id, builders in command_builders.items():
+            await bot.rest.set_application_commands(
+                application=application.id,
+                commands=builders,
+                guild=int(server_id) if server_id != ""
+                      else self.__developer_server_id if self.__debugger.is_debug_mode_enabled()
+                      else hikari.UNDEFINED
+            )
+
+        self.__logger.info("Registered all application commands")

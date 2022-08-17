5d3a966b825c9a16f09d7d83717a9ed8dec03d85
tooru <tooru@tooru.ga>
2022-08-17 14:29:15 +0300

    Rename list param because Rexor doesn't like pre/suffixing variable names despite using double underscore in almost every part of code

--- holobot/sdk/utils/list_utils.py
+++ holobot/sdk/utils/list_utils.py
@@ -2,8 +2,8 @@ from typing import TypeVar
 
 T = TypeVar("T")
 
-def pad_left(list_: list[T], value: T, length: int) -> list[T]:
+def pad_left(source: list[T], value: T, length: int) -> list[T]:
     if length < 0:
         raise ValueError("Length cannot be less than zero.")
-    length = max(length, len(list_))
-    return [value] * (length - len(list_)) + list_
+    length = max(length, len(source))
+    return [value] * (length - len(source)) + source

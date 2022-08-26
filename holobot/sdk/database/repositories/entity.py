ec4aa180938755c500d0a423b5f710300859c738
Rexor <rrexor@gmail.com>
2022-08-26 11:17:53 +0200

    Turn Entity into an abstract base dataclass

--- holobot/sdk/database/repositories/entity.py
+++ holobot/sdk/database/repositories/entity.py
@@ -1,13 +1,11 @@
-from typing import Protocol, TypeVar
+from abc import ABC
+from dataclasses import dataclass
+from typing import Generic, TypeVar
 
 TIdentifier = TypeVar("TIdentifier")
 
-class Entity(Protocol[TIdentifier]):
+@dataclass(kw_only=True)
+class Entity(ABC, Generic[TIdentifier]):
     # NOTE By convention, only simple primary keys are supported
     # and they must be named as "id".
     id: TIdentifier
-
-    # NOTE This empty initializer is required to avoid a false error
-    # in RepositoryBase that believes IEntity has no initializer.
-    def __init__(self) -> None:
-        super().__init__()

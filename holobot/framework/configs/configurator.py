c16bcf8a5f850c39df297c0e2a4899ec02c7d609
tooru <tooru@tooru.ga>
2022-09-02 12:23:57 +0300

    Address PR comment

--- holobot/framework/configs/configurator.py
+++ holobot/framework/configs/configurator.py
@@ -29,7 +29,7 @@ class Configurator(ConfiguratorInterface):
 
     @staticmethod
     def __get_env(section_name: str, parameter_name: str, default_value: TValue) -> TValue | None:
-        if not (value := os.environ.get(f"{section_name}-{parameter_name}")):
+        if (value := os.environ.get(f"{section_name}-{parameter_name}")) is None:
             return None
         # Because of this check, only the string/bool/int queries are supported, technically.
         match default_value:

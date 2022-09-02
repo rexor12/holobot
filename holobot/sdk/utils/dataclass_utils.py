08e597f2974cbef4274d9e82d41aa2eb5f2a3337
tooru <tooru@tooru.ga>
2022-09-02 12:24:29 +0300

    Fix pattern matching

--- holobot/sdk/utils/dataclass_utils.py
+++ holobot/sdk/utils/dataclass_utils.py
@@ -1,18 +1,19 @@
+import builtins
+import types
+import typing
 from collections.abc import Sequence
 from dataclasses import _MISSING_TYPE, fields, is_dataclass
-from types import UnionType
-from typing import Any, Callable, NamedTuple, Union, cast, get_args, get_origin, get_type_hints
 
 from holobot.sdk.exceptions import ArgumentError
 from holobot.sdk.utils import first
 
-class ArgumentInfo(NamedTuple):
+class ArgumentInfo(typing.NamedTuple):
     name: str
     object_type: type
     collection_constructor: type | None
     allows_none: bool
-    default_value: Any
-    default_factory: Callable[[], Any] | None
+    default_value: typing.Any
+    default_factory: typing.Callable[[], typing.Any] | None
 
 def get_argument_infos(dataclass_type: type) -> Sequence[ArgumentInfo]:
     if not is_dataclass(dataclass_type):
@@ -24,7 +25,7 @@ def get_argument_infos(dataclass_type: type) -> Sequence[ArgumentInfo]:
 
     # Need to explicitly load type hints for dataclasses.
     # See: https://stackoverflow.com/a/55938344
-    resolved_type_hints = get_type_hints(initializer)
+    resolved_type_hints = typing.get_type_hints(initializer)
 
     return [
         __get_argument_info(
@@ -38,26 +39,26 @@ def get_argument_infos(dataclass_type: type) -> Sequence[ArgumentInfo]:
 def __get_argument_info(
     name: str,
     object_type: type,
-    default_value: Any,
-    default_factory: Any
+    default_value: typing.Any,
+    default_factory: typing.Any
 ) -> ArgumentInfo:
-    match origin := get_origin(object_type):
+    match origin := typing.get_origin(object_type):
         case None:
             return ArgumentInfo(name, object_type, None, False, default_value, default_factory)
-        case Union() | UnionType():
-            args = get_args(object_type)
+        case typing.Union | types.UnionType:
+            args = typing.get_args(object_type)
             if len(args) != 2 or None not in args:
-                raise ValueError(f"Expected a union with two arguments, the second being None, but got {args!r} instead.")
-            object_type = first(cast(tuple[type, ...], args), lambda i: i and i is not None)
-            origin = get_origin(object_type)
+                raise ValueError(f"Expected a Union with two arguments, the second being None, but got {args!r} instead.")
+            object_type = first(typing.cast(tuple[type, ...], args), lambda i: i and i is not None)
+            origin = typing.get_origin(object_type)
 
     match origin:
         case None:
             return ArgumentInfo(name, object_type, None, True, default_value, default_factory)
-        case tuple() | list():
-            args = get_args(object_type)
-            if isinstance(origin, tuple) and (len(args) != 2 or args[1] is not Ellipsis):
+        case builtins.tuple | builtins.list:  # Needs dot notation: https://peps.python.org/pep-0634/#value-patterns
+            args = typing.get_args(object_type)
+            if origin is tuple and (len(args) != 2 or args[1] is not Ellipsis):
                 raise ValueError(f"Expected a tuple with two arguments, the second being Ellipsis, but got {args!r} instead.")
-            return ArgumentInfo(name, args[0], type(origin), True, default_value, default_factory)
+            return ArgumentInfo(name, args[0], origin, True, default_value, default_factory)
         case _:
             raise ValueError(f"Expected None, tuple or list type, but got '{object_type}'.")

00fb89c7d496fd1643c8056a0567d58dae261f7f
rexor12 <rrexor@gmail.com>
2023-01-22 18:04:00 +0100

    Replace text links with link buttons

--- holobot/extensions/general/workflows/view_bot_info_workflow.py
+++ holobot/extensions/general/workflows/view_bot_info_workflow.py
@@ -2,10 +2,13 @@ from datetime import datetime
 
 import tzlocal
 
-from holobot.discord.sdk.actions import ReplyAction
 from holobot.discord.sdk.data_providers import IBotDataProvider
-from holobot.discord.sdk.models import Embed, EmbedField, EmbedFooter
+from holobot.discord.sdk.models import Embed, EmbedFooter
 from holobot.discord.sdk.workflows import IWorkflow, WorkflowBase
+from holobot.discord.sdk.workflows.interactables.components import (
+    Button, ComponentBase, StackLayout
+)
+from holobot.discord.sdk.workflows.interactables.components.enums import ComponentStyle
 from holobot.discord.sdk.workflows.interactables.decorators import command
 from holobot.discord.sdk.workflows.interactables.models import InteractionResponse
 from holobot.discord.sdk.workflows.models import ServerChatInteractionContext
@@ -32,51 +35,60 @@ class ViewBotInfoWorkflow(WorkflowBase):
         context: ServerChatInteractionContext
     ) -> InteractionResponse:
         current_time = datetime.now(tzlocal.get_localzone())
+        buttons: list[ComponentBase] = [
+            Button(
+                id="project_home",
+                owner_id=context.author_id,
+                text=self.__i18n_provider.get(
+                    "extensions.general.view_bot_info_workflow.project_home"
+                ),
+                style=ComponentStyle.LINK,
+                url="https://github.com/rexor12/holobot"
+            ),
+            Button(
+                id="report_bug",
+                owner_id=context.author_id,
+                text=self.__i18n_provider.get(
+                    "extensions.general.view_bot_info_workflow.report_bug"
+                ),
+                style=ComponentStyle.LINK,
+                url="https://github.com/rexor12/holobot/issues/new?assignees=&labels=bug%2Fbugfix&template=bug_report.yml"
+            ),
+            Button(
+                id="request_feature",
+                owner_id=context.author_id,
+                text=self.__i18n_provider.get(
+                    "extensions.general.view_bot_info_workflow.request_feature"
+                ),
+                style=ComponentStyle.LINK,
+                url="https://github.com/rexor12/holobot/issues/new?assignees=&labels=feature+request&template=feature_request.yml"
+            )
+        ]
+
         return self._reply(
             embed=Embed(
                 title=self.__i18n_provider.get(
                     "extensions.general.view_bot_info_workflow.embed_title"
                 ),
                 description=self.__i18n_provider.get(
-                    "extensions.general.view_bot_info_workflow.embed_description"
+                    "extensions.general.view_bot_info_workflow.general_value",
+                    {
+                        "version": str(self.__environment.version),
+                        "latency": self.__bot_data_provider.get_latency(),
+                        "servers": self.__bot_data_provider.get_server_count(),
+                        "server_time": current_time
+                    }
                 ),
                 thumbnail_url=self.__bot_data_provider.get_avatar_url(),
-                fields=[
-                    EmbedField(
-                        self.__i18n_provider.get(
-                            "extensions.general.view_bot_info_workflow.general_field"
-                        ),
-                        self.__i18n_provider.get(
-                            "extensions.general.view_bot_info_workflow.general_value",
-                            {
-                                "version": str(self.__environment.version),
-                                "latency": self.__bot_data_provider.get_latency(),
-                                "servers": self.__bot_data_provider.get_server_count(),
-                                "server_time": current_time
-                            }
-                        ),
-                        False
-                    ),
-                    EmbedField(
-                        self.__i18n_provider.get(
-                            "extensions.general.view_bot_info_workflow.project_home"
-                        ),
-                        "https://github.com/rexor12/holobot",
-                        False
-                    ),
-                    EmbedField(
-                        self.__i18n_provider.get(
-                            "extensions.general.view_bot_info_workflow.bug_reports"
-                        ),
-                        "https://github.com/rexor12/holobot/issues",
-                        False
-                    )
-                ],
                 footer=EmbedFooter(
                     self.__i18n_provider.get(
                         "extensions.general.view_bot_info_workflow.embed_footer"
                     ),
                     "https://avatars.githubusercontent.com/u/15330052"
                 )
+            ),
+            components=StackLayout(
+                id="bot_info_buttons",
+                children=buttons
             )
         )
